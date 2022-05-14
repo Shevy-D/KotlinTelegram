2d5eba5d01f5d584b6493397b47eae161bffdacb
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-05-14 18:12:54 +0500

    Added ability to change bio

---
+++ app/src/main/java/com/shevy/kotlintelegram/ui/fragments/ChangeBioFragment.kt
@@ -0,0 +1,28 @@
+package com.shevy.kotlintelegram.ui.fragments
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import com.shevy.kotlintelegram.MainActivity
+import com.shevy.kotlintelegram.R
+import com.shevy.kotlintelegram.utilits.*
+import kotlinx.android.synthetic.main.fragment_change_bio.*
+
+class ChangeBioFragment : BaseChangeFragment(R.layout.fragment_change_bio) {
+    override fun onResume() {
+        super.onResume()
+        settings_input_bio.setText(USER.bio)
+    }
+
+    override fun change() {
+        super.change()
+        val newBio = settings_input_bio.text.toString()
+        REF_DATABASE_ROOT.child(NODE_USERS).child(UID).child(CHILD_BIO).setValue(newBio).addOnCompleteListener {
+            if (it.isSuccessful) {
+                showToast(getString(R.string.toast_data_update))
+                USER.bio = newBio
+                fragmentManager?.popBackStack()
+            }
+        }
+    }
+}
