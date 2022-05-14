984e109fa7de6af1e029c4ca6765ab0ff7fe4eea
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-05-14 15:00:52 +0500

    Added initialization user model

--- app/src/main/java/com/shevy/kotlintelegram/MainActivity.kt
+++ app/src/main/java/com/shevy/kotlintelegram/MainActivity.kt
@@ -4,14 +4,15 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import com.shevy.kotlintelegram.activities.RegisterActivity
 import com.shevy.kotlintelegram.databinding.ActivityMainBinding
+import com.shevy.kotlintelegram.models.User
 import com.shevy.kotlintelegram.ui.fragments.ChatFragment
 import com.shevy.kotlintelegram.ui.objects.AppDrawer
-import com.shevy.kotlintelegram.utilits.AUTH
-import com.shevy.kotlintelegram.utilits.initFirebase
-import com.shevy.kotlintelegram.utilits.replaceActivity
-import com.shevy.kotlintelegram.utilits.replaceFragment
+import com.shevy.kotlintelegram.utilits.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -45,5 +46,13 @@ class MainActivity : AppCompatActivity() {
         mToolbar = mBinding.mainToolbar
         mAppDrawer = AppDrawer(this, mToolbar)
         initFirebase()
+        initUser()
+    }
+
+    private fun initUser() {
+        REF_DATABASE_ROOT.child(NODE_USERS).child(UID)
+            .addListenerForSingleValueEvent(AppValueEventListener {
+                USER = it.getValue(User::class.java) ?: User()
+            })
     }
 }
