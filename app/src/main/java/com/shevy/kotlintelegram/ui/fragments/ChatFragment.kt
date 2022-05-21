2903d34048c0f202c8a4c1a15c36f834b306ff6d
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-05-21 11:20:09 +0500

    Added sending messages to Firebase

--- app/src/main/java/com/shevy/kotlintelegram/ui/fragments/ChatFragment.kt
+++ app/src/main/java/com/shevy/kotlintelegram/ui/fragments/ChatFragment.kt
@@ -2,10 +2,12 @@ package com.shevy.kotlintelegram.ui.fragments
 
 import androidx.fragment.app.Fragment
 import com.shevy.kotlintelegram.R
+import com.shevy.kotlintelegram.utilits.APP_ACTIVITY
 
 class ChatFragment : Fragment(R.layout.fragment_chat) {
 
     override fun onResume() {
         super.onResume()
+        APP_ACTIVITY.title = "Чаты"
     }
 }
--- app/src/main/java/com/shevy/kotlintelegram/ui/fragments/SingleChatFragment.kt
+++ app/src/main/java/com/shevy/kotlintelegram/ui/fragments/SingleChatFragment.kt
@@ -7,6 +7,7 @@ import com.shevy.kotlintelegram.models.CommonModel
 import com.shevy.kotlintelegram.models.UserModel
 import com.shevy.kotlintelegram.utilits.*
 import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.fragment_single_chat.*
 import kotlinx.android.synthetic.main.toolbar_info.view.*
 
 class SingleChatFragment(private val contact: CommonModel) : BaseFragment(R.layout.fragment_single_chat) {
@@ -27,6 +28,15 @@ class SingleChatFragment(private val contact: CommonModel) : BaseFragment(R.layo
 
         mRefUser = REF_DATABASE_ROOT.child(NODE_USERS).child(contact.id)
         mRefUser.addValueEventListener(mListenerInfoToolbar)
+        chat_btn_send_message.setOnClickListener {
+            val message = chat_input_message.text.toString()
+            if (message.isEmpty()) {
+                showToast("Введите сообщение")
+            } else sendMessage(message, contact.id, TYPE_TEXT) {
+                chat_input_message.setText("")
+            }
+
+        }
     }
 
     private fun initInfoToolbar() {
