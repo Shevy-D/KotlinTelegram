1ca0851d3157d645a87bf4b5237885e8840f7643
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-05-22 14:23:16 +0500

    Added sending pictures to chat

--- app/src/main/java/com/shevy/kotlintelegram/utilits/appConstants.kt
+++ app/src/main/java/com/shevy/kotlintelegram/utilits/appConstants.kt
@@ -3,3 +3,5 @@ package com.shevy.kotlintelegram.utilits
 import com.shevy.kotlintelegram.MainActivity
 
 lateinit var APP_ACTIVITY: MainActivity
+const val TYPE_MESSAGE_IMAGE = "image"
+const val TYPE_MESSAGE_TEXT = "text"
