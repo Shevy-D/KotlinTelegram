3c641be3ae49a1819eef3571b6ca88d241781fed
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-05-28 13:07:50 +0500

    Added implementation of groups. Part 2.

--- app/src/main/java/com/shevy/kotlintelegram/ui/screens/groups/AddContactsFragment.kt
+++ app/src/main/java/com/shevy/kotlintelegram/ui/screens/groups/AddContactsFragment.kt
@@ -9,6 +9,7 @@ import com.shevy.kotlintelegram.models.CommonModel
 import com.shevy.kotlintelegram.utilits.APP_ACTIVITY
 import com.shevy.kotlintelegram.utilits.AppValueEventListener
 import com.shevy.kotlintelegram.utilits.hideKeyBoard
+import com.shevy.kotlintelegram.utilits.replaceFragment
 import kotlinx.android.synthetic.main.fragment_add_contacts.*
 import kotlinx.android.synthetic.main.fragment_main_list.*
 
@@ -31,6 +32,7 @@ class AddContactsFragment : Fragment(R.layout.fragment_add_contacts) {
             listContacts.forEach {
                 println(it.id)
             }
+            replaceFragment(CreateGroupFragment(listContacts))
         }
     }
 
---
+++ app/src/main/java/com/shevy/kotlintelegram/ui/screens/groups/CreateGroupFragment.kt
@@ -0,0 +1,34 @@
+package com.shevy.kotlintelegram.ui.screens.groups
+
+import androidx.recyclerview.widget.RecyclerView
+import com.mikepenz.materialize.util.KeyboardUtil.hideKeyboard
+import com.shevy.kotlintelegram.R
+import com.shevy.kotlintelegram.models.CommonModel
+import com.shevy.kotlintelegram.ui.screens.base.BaseFragment
+import com.shevy.kotlintelegram.utilits.APP_ACTIVITY
+import com.shevy.kotlintelegram.utilits.hideKeyBoard
+import com.shevy.kotlintelegram.utilits.showToast
+import kotlinx.android.synthetic.main.fragment_create_group.*
+
+class CreateGroupFragment(private var listContacts:List<CommonModel>): BaseFragment(R.layout.fragment_create_group) {
+
+    private lateinit var mRecyclerView: RecyclerView
+    private lateinit var mAdapter: AddContactsAdapter
+
+    override fun onResume() {
+        super.onResume()
+        APP_ACTIVITY.title = getString(R.string.create_group)
+        APP_ACTIVITY.mAppDrawer.enableDrawer()
+        hideKeyBoard()
+        initRecyclerView()
+        create_group_btn_complete.setOnClickListener { showToast("Click") }
+        create_group_input_name.requestFocus()
+    }
+
+    private fun initRecyclerView() {
+        mRecyclerView = create_group_recycle_view
+        mAdapter = AddContactsAdapter()
+        mRecyclerView.adapter = mAdapter
+        listContacts.forEach {  mAdapter.updateListItems(it) }
+    }
+}
